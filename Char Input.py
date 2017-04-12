6b0fed7cd9c4738ddb4a727f5ae032fffbff42df
pnadolny13 <patnadolny@gmail.com>
2017-04-12 18:28:37 -0400

    Add files via upload

---
+++ Char Input.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 20:57:57 2016
+
+@author: pnadolny
+"""
+
+name = input("What is your name? ")
+print ("Hi " + name)
+
+age = int(input("What is your age? "))
+    
+print ("You will be 100 years old in " + str(2116-age))
