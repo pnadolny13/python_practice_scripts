f10f3d991f9254556a4d02e6bc27ab57e067995e
pnadolny13 <patnadolny@gmail.com>
2017-05-31 15:30:05 -0400

    Update FilterResultsExport.py

--- FilterResultsExport.py
+++ FilterResultsExport.py
@@ -10,12 +10,11 @@ mydata.count()
 
 today_long = datetime.datetime.now()
 today = (today_long.date()).strftime("%-m/%-d/%Y")
-day_1 = ((today_long - datetime.timedelta(days =1)).date()).strftime("%-m/%-d/%Y")
-day_2 = ((today_long - datetime.timedelta(days =2)).date()).strftime("%-m/%-d/%Y")
-day_3 = ((today_long - datetime.timedelta(days =3)).date()).strftime("%-m/%-d/%Y")
-day_4 = ((today_long - datetime.timedelta(days =4)).date()).strftime("%-m/%-d/%Y")
-day_5 = ((today_long - datetime.timedelta(days =5)).date()).strftime("%-m/%-d/%Y")
-day_6 = ((today_long - datetime.timedelta(days =6)).date()).strftime("%-m/%-d/%Y")
+day_list = [1,2,3,4,5,6];
+count = 1;
+for day in day_list:
+  "day_"+ str(count) = ((today_long - datetime.timedelta(days =count)).date()).strftime("%-m/%-d/%Y")
+  count = count +1
 
 mydata_filt= mydata.filter(lambda x: today in x or day_1 in x or day_2 in x or day_3 in x or day_4 in x or day_5 in x or day_6 in x)
 
